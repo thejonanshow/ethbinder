b3c71ccd300de5352ff7bf373595703d1d598211
Jonan Scheffler <jonanscheffler@gmail.com>
2024-09-16 15:07:06 +0200

    Badge response work! (?)

--- eth-verification-worker/src/index.ts
+++ eth-verification-worker/src/index.ts
@@ -37,23 +37,21 @@ export default {
       return null;
     };
 
-    const sendResponse = (message: string, options: Object) => {
-      const respData = debug ? { message, logs } : { message }
-      return new Response(JSON.stringify(respData), options)
-    }
-
-    const sendBadgeResponse = (isVerified: string, options: Object) => {
-      const badgeData = {
+    const buildBadge = (badgeMessage: string, success: Boolean, options: Object) => {
+      return {
+        schemaVersion: 1,
         label: 'ethbinder',
-        message: isVerified ? 'verified' : 'failed',  // Dynamic based on verification
-        labelColor: '#555',  // Optional label color
-        color: isVerified ? '#4c1' : '#e05d44',  // Green for verified, red for invalid
-        logo: 'ethereum',
-        links: ['https://github.com/thejonanshow/ethbinder'],  // Optional links
+        message: badgeMessage,
+        labelColor: '#5177D0',  // Optional label color
+        color: success ? '#51D06A' : '#D06A51',  // Green for verified, red for invalid
+        namedLogo: 'ethereum',
         style: 'flat'  // Optional badge style
       };
+    }
 
-      const respData = debug ? { badgeData, logs } : { badgeData }
+    const sendBadgeResponse = (message: string, success: Boolean, options: Object) => {
+      const badgeData = buildBadge(message, success, options);
+      const respData = debug ? { ...badgeData, logs } : { ...badgeData }
       return new Response(JSON.stringify(respData), options)
     }
 
@@ -62,10 +60,38 @@ export default {
 
     if (!githubHandle || !repo) {
       logs.push('Missing handle or repo query parameters');
-      return sendResponse(
-        "Missing handle or repo query parameters",
+      return sendBadgeResponse(
+        "missing handle",
+        false,
         { status: 400, headers: { 'Content-Type': 'application/json' }}
       );
+    }
+
+    const validHandle = githubHandle ? await verifyGitHubHandle(githubHandle) : false;
+    if (validHandle) {
+      logs.push(`Handle is valid, validHandle: ${JSON.stringify(validHandle)}, handle: ${githubHandle}`);
+    } else {
+      return sendBadgeResponse(
+        `invalid handle: ${githubHandle}`,
+        false,
+        { status: 500, headers: { 'Content-Type': 'application/json' }}
+      );
+    }
+
+    if (validHandle) {
+      const validRepo = await verifyRepo(githubHandle);
+        logs.push(`verifyRepo result: ${JSON.stringify(validRepo)}`);
+
+      if (validRepo === true) {
+        logs.push(`User has an ethbinder repo: ${githubHandle}`);
+      } else {
+        logs.push(`User ${githubHandle} does not have an ethbinder repo`);
+        return sendBadgeResponse(
+          `missing repo ethbinder`,
+          false,
+          { status: 500, headers: { 'Content-Type': 'application/json' }}
+        );
+      }
     }
 
     const githubToken = env.GITHUB_API_KEY;
@@ -73,11 +99,111 @@ export default {
 
     if (!githubToken) {
       logs.push('GitHub token is not set');
-      return sendResponse(
-        "GitHub token is not set",
+      return sendBadgeResponse(
+        "missing github token",
+        false,
         { status: 500, headers: { 'Content-Type': 'application/json' }}
       );
     }
+
+    async function verifyGitHubHandle(handle: string) {
+      const url = `https://api.github.com/users/${handle}`;
+
+      try {
+        const response = await fetch(url, {
+          method: 'GET',
+          headers: {
+            'Accept': 'application/vnd.github.v3+json',
+            'User-Agent': 'YourAppName',  // GitHub API requires a user-agent
+          },
+        });
+
+        // Await and log the actual JSON response
+        const responseData = await response.json();
+        logs.push(`GitHub handle verification response ${response.status}: ${JSON.stringify(responseData)}`);
+
+        // Check if the user exists by looking at the status code
+        if (response.status === 200) {
+          logs.push(`GitHub user ${handle} found`);
+          return true;
+        } else {
+          logs.push(`GitHub user ${handle} not found. Status: ${response.status}`);
+          return false;
+        }
+      } catch (error) {
+        logs.push(`Error while verifying GitHub handle ${handle}: ${error.message}`);
+        return false;
+      }
+    }
+
+
+    async function verifyRepo(handle: string) {
+      const baseUrl = `https://api.github.com/users/${handle}/repos`;
+      let page = 1;
+      let repos = [];
+      let hasEthBinderRepo = false;
+
+      logs.push(`Fetching repos for handle: ${handle} from ${baseUrl}`);
+
+      try {
+        while (true) {
+          const url = `${baseUrl}?per_page=100&page=${page}`;
+          logs.push(`Fetching page ${page} from ${url}`);
+
+          const response = await fetch(url, {
+            method: 'GET',
+            headers: {
+              'Accept': 'application/vnd.github.v3+json',
+              'User-Agent': 'YourAppName',  // GitHub API requires a user-agent
+            },
+          });
+
+          logs.push(`GitHub API response status for user ${handle} on page ${page}: ${response.status}`);
+
+          if (response.status !== 200) {
+            if (response.status === 404) {
+              logs.push(`GitHub user ${handle} not found (404)`);
+            } else {
+              logs.push(`Error: GitHub API returned status ${response.status} for user ${handle}`);
+            }
+            return false;
+          }
+
+          const pageRepos = await response.json();
+          logs.push(`Fetched ${pageRepos.length} repositories on page ${page}`);
+
+          repos = repos.concat(pageRepos);
+
+          // Check if any repo is named 'ethbinder' (on the current page)
+          if (pageRepos.some(repo => repo.name.toLowerCase() === 'ethbinder')) {
+            logs.push(`User ${handle} has an ethbinder repository`);
+            hasEthBinderRepo = true;
+            break;
+          }
+
+          // If fewer than 100 repos were fetched, there are no more pages
+          if (pageRepos.length < 100) {
+            break;
+          }
+
+          page++;
+        }
+
+        logs.push(`User ${handle} has a total of ${repos.length} repositories`);
+        logs.push(`Repositories: ${repos.map(r => r.name.toLowerCase()).join(', ')}`);
+
+        if (!hasEthBinderRepo) {
+          logs.push(`User ${handle} does not have an ethbinder repository`);
+        }
+
+        return hasEthBinderRepo;
+
+      } catch (error) {
+        logs.push(`Error while fetching repos for user ${handle}: ${error.message}`);
+        return false;
+      }
+    }
+
 
     // Fetch issues from GitHub repo
     logs.push(`Fetching issues from GitHub for repo: ${githubHandle}/${repo}`);
@@ -96,8 +222,8 @@ export default {
       logs.push(`Failed to fetch issues from GitHub: ${response.statusText} (${response.status})`);
       logs.push(`Error body: ${errorText}`);
 
-      return sendResponse(
-        `Failed to fetch issues from GitHub: ${response.statusText}`,
+      return sendBadgeResponse(
+        `issue fetch failed: ${response.statusText}`,
         { status: 500, headers: { 'Content-Type': 'application/json' } }
       );
     }
@@ -131,12 +257,14 @@ export default {
             if (recoveredAddress.toLowerCase() === address.toLowerCase()) {
               logs.push('Signature verified successfully.');
               return sendBadgeResponse(
+                "verified",
                 true,
                 { status: 200, headers: { 'Content-Type': 'application/json' } }
               );
             } else {
               logs.push('Signature verification failed.');
               return sendBadgeResponse(
+                "failed",
                 false,
                 { status: 400, headers: { 'Content-Type': 'application/json' } }
               );
@@ -153,8 +281,9 @@ export default {
     }
 
     logs.push("No valid issue found with signature verification.");
-    return sendResponse(
-      'No valid issue found with signature verification.',
+    return sendBadgeResponse(
+      'no issue found',
+      false,
       { status: 404, headers: { 'Content-Type': 'application/json' } }
     );
   },
